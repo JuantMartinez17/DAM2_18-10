f5a9fbcb816a4a024631581e4920323685ed7e5a
JuantMartinez17 <juantomasm0@gmail.com>
2024-10-18 19:56:06 -0300

    data class and adapter created

---
+++ app/src/main/java/com/example/dam2_18_10/adapter/AlumnoViewHolder.kt
@@ -0,0 +1,18 @@
+package com.example.dam2_18_10.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dam2_18_10.data.Alumno
+import com.example.dam2_18_10.databinding.AlumnoItemBinding
+
+class AlumnoViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    val binding:AlumnoItemBinding = AlumnoItemBinding.bind(view)
+
+    fun render(alumno: Alumno){
+
+    }
+
+    fun bind(alumno: Alumno){
+
+    }
+}
---
+++ app/src/main/java/com/example/dam2_18_10/data/Alumno.kt
@@ -0,0 +1,10 @@
+package com.example.dam2_18_10.data
+
+data class Alumno(
+    val id: Int,
+    val name: String,
+    val lastName: String,
+    val dni: Int,
+    val city: String,
+    val cp: String,
+)
